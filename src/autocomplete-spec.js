b196bfa3f6787532f03803c78490f420cfebca30
francisu <francisu@gmail.com>
2021-09-21 12:59:43 -0700

    create-react-selector repro issue #235

--- src/autocomplete-spec.js
+++ src/autocomplete-spec.js
@@ -9,41 +9,71 @@ import TextField from '@material-ui/core/TextField'
 import Autocomplete from '@material-ui/lab/Autocomplete'
 import { top100Films } from './top-100-movies'
 
-it('finds my favorite movie', () => {
-  cy.viewport(500, 700)
-  cy.waitForReact(5000, '#root');
-  mount(
-    <Autocomplete
-      id="combo-box-demo"
-      options={top100Films}
-      getOptionLabel={option => option.title}
-      style={{ width: 300 }}
-      renderInput={params => (
-        <TextField {...params} label="Combo box" variant="outlined" fullWidth />
-      )}
-    />,
-    {
-      stylesheets: [
-        'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap',
-        'https://fonts.googleapis.com/icon?family=Material+Icons',
-      ],
-    },
-  )
+describe('multiple tests', () =>{
+  it('finds my favorite movie', () => {
+    cy.viewport(500, 700)
+    mount(
+      <Autocomplete
+          id="combo-box-demo"
+          options={top100Films}
+          getOptionLabel={option => option.title}
+          style={{ width: 300 }}
+          renderInput={params => (
+              <TextField {...params} label="Combo box" variant="outlined" fullWidth />
+          )}
+      />,
+      {
+        stylesheets: [
+          'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap',
+          'https://fonts.googleapis.com/icon?family=Material+Icons',
+        ],
+      },
+    )
 
-  cy.get('#combo-box-demo').click()
-  cy.focused().type('god')
-  cy.contains('The Godfather')
-    .should('be.visible')
-    .and('have.class', 'MuiAutocomplete-option')
-    .click()
-  cy.get('#combo-box-demo').should('have.value', 'The Godfather')
+    cy.waitForReact();
+    cy.react('Autocomplete').should('exist');
 
-  // now delete a couple of letter
-  cy.focused().type('{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}')
-  // should show two search results
-  cy.get('.MuiAutocomplete-option').should('have.length', 2)
-    .and(el$ => {
-      expect(el$[0]).to.have.text('The Godfather')
-      expect(el$[1]).to.have.text('The Godfather: Part II')
-    })
-})
+    cy.get('#combo-box-demo').click()
+    cy.focused().type('god')
+    cy.contains('The Godfather')
+        .should('be.visible')
+        .and('have.class', 'MuiAutocomplete-option')
+        .click()
+    cy.get('#combo-box-demo').should('have.value', 'The Godfather')
+
+    // now delete a couple of letter
+    cy.focused().type('{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}')
+    // should show two search results
+    cy.get('.MuiAutocomplete-option').should('have.length', 2)
+        .and(el$ => {
+          expect(el$[0]).to.have.text('The Godfather')
+          expect(el$[1]).to.have.text('The Godfather: Part II')
+        })
+  });
+
+  it('finds my favorite movie - redux', () => {
+    cy.viewport(500, 700)
+    mount(
+        <Autocomplete
+            id="combo-box-demo"
+            options={top100Films}
+            getOptionLabel={option => option.title}
+            style={{width: 300}}
+            renderInput={params => (
+                <TextField {...params} label="Combo box" variant="outlined" fullWidth/>
+            )}
+        />,
+        {
+          stylesheets: [
+            'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap',
+            'https://fonts.googleapis.com/icon?family=Material+Icons',
+          ],
+        },
+    )
+
+    cy.waitForReact();
+    cy.react('Autocomplete').should('exist');
+
+  });
+
+});
